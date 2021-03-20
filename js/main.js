73f528363a9df1f8160d4e6f9e3f70e461ccbecc
Small-hawk <kakaste92@gmail.com>
2021-03-20 03:18:39 +0200

    ✅ Completed. Ready JS file.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
+'use strict';
 
+function alertMsg(){
+    alert('Please push one of the keys belows in your keyboard!');
+}
 
-document.addEventListener('keydown', function(e){
-    console.log(e.code);
-    console.log(e.key);
-})
+function playDrum(theKey = ""){
+    let audio = document.querySelector(`audio[data-key *= "${theKey.toUpperCase()}"]`);
+    if (!audio){
+        alertMsg();
+        return;
+    }
+    showEffect(theKey);
+    audio.play();
+    audio.currentTime = 0;
+}
+
+function showEffect(theKey = ""){
+    let effect = document.querySelector(`div[data-key *= ${theKey.toUpperCase()}]`);
+    effect.classList.add('playing');
+}
+
+function removeTransition(theKey){
+    if (!theKey.propertyName.includes('transform')){
+        return;
+    }
+    this.classList.remove('playing');
+}
+
+document.addEventListener('keydown', evt => playDrum(evt.key));
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(x => {x.addEventListener('transitionend', removeTransition)});
